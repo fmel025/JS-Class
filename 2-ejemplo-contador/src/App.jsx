5426ba9b7a02522639807e7c4eb92127c6469d79
fmel025 <00018720@uca.edu.sv>
2023-07-06 19:44:57 -0600

    :sparkles: Add counter example

---
+++ 2-ejemplo-contador/src/App.jsx
@@ -0,0 +1,25 @@
+import Login from "./Login"
+import Counter from "./Counter"
+
+// Componente funcional sintaxis:
+
+// function nombreDelComponente()
+// El return devuelve el html que queremos mostrar en pantalla
+
+// main.jsx renderiza la aplicacion entera, y lo que esta dentro
+// de el sera renderizado en pantalla
+// App.jsx es un componente que se renderiza dentro de main.jsx
+// App.jsx es un componente funcional, es decir, una funcion
+// que devuelve un html
+
+// Tarea: Investigar sobre la estructura de carpetas de un proyecto en react
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
