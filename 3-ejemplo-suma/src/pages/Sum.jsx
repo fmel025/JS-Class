baa1411eb6d089125653085ebce27426b67ca8c6
fmel025 <00018720@uca.edu.sv>
2023-07-12 07:10:21 -0600

    :sparkles: Add sum example using useMemo

--- 3-ejemplo-suma/src/pages/Sum.jsx
+++ 3-ejemplo-suma/src/pages/Sum.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 // useState se utiliza para crear estados (variables) que son propias del componente los cuales nos ayudan a renderizar o actualizar las vistas o permitirnos a hacer ciertas cosas cuando estas tengan un cierto valor. useState se ejecuta al inicio de la carga de un componente.
 
@@ -8,23 +8,40 @@ import { useState, useEffect } from "react";
 const Sum = () => {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-  const [sum, setSum] = useState(() => num1 + num2);
+
+  // hooks: Son funciones en react que nos permiten hacer ciertas cosas. Como manejar estados, manipular datos de un componente durante algun determinado tiempo en el ciclo de vida, etc.
+
+  // useMemo no siempre es recomendado usarlo (ahorita solo es para dar el ejemplo), este hook sirve cuando necesitamos obtener un valor resultante de un calculo el cual es muy complejo, para que este calculo solo se ejecute, cuando los datos del arreglo de dependencias cambien, en ese momento el dato se va a recalcular.
+  const sum = useMemo(() => num1 + num2, [num1, num2]);
+
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  // Si el arreglo de dependencias del use effect esta vacio, solo se ejecutara el useEffect cuando el componente se cargue por primera vez. Si el arreglo de dependencias tiene valores, entonces el useEffect se ejecutara cada vez que uno de esos valores cambie.
 
   // Ahora incluiremos useEffect
   // useEffect lo que hace es que esto se ejecuta cuando se crea un componente (antes del render y cuando se actualicen ciertos valores)
 
-  useEffect(() => {
-    setSum(num1 + num2);
-  }, [num1, num2]);
+  // useEffect pide un arreglo de datos en los cuales el re ejecutara la funcion si los valores de ese arreglo cambian.
+  // useEffect se usa cuando necesito ejecutar una funcion cada vez que un valor cambie o cuando necesito realizar operaciones para cargar datos antes de que el componente se cargue.
 
+
+  // Si solo necesito ejecutarlo una vez, entonces el arreglo de dependencias debe estar vacio.
+
+  // || se ejecutara siempre y cuando uno de los valores sea nulo
+  // En nuestro ejemplo si el input esta vacio lo que estoy diciendo es que el valor que voy a a asignar es un 0.
   const handleChangeNum1 = (event) => {
     event.preventDefault();
-    setNum1(Number(event.target.value));
+    setNum1(Number(event.target.value || 0));
   };
 
   const handleChangeNum2 = (event) => {
     event.preventDefault();
-    setNum2(Number(event.target.value));
+    setNum2(Number(event.target.value || 0));
   };
 
   return (
@@ -40,7 +57,7 @@ const Sum = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="number"
+              type="text"
               name="num1"
               id="num1"
               placeholder="Ingresa un numero"
@@ -57,7 +74,7 @@ const Sum = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="number"
+              type="text"
               name="num2"
               id="num2"
               placeholder="Ingresa un numero"
@@ -78,3 +95,7 @@ const Sum = () => {
 };
 
 export default Sum;
+
+
+// Tarea:
+// Agregar un input extra para que el usuario pueda ingresar un numero 3 y mostrar la multiplicacion de los 3 numeros en pantalla.
