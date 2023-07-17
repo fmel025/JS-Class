9daba47151c7c68ce2308fd4e2987a26085319bf
fmel025 <00018720@uca.edu.sv>
2023-07-17 17:27:35 -0600

    :memo: Add notes for react hooks

--- 4-ejemplo-fetch/src/components/UserCard.jsx
+++ 4-ejemplo-fetch/src/components/UserCard.jsx
@@ -1,4 +1,16 @@
-const UserCard = ({ username, email, avatarUrl }) => {
+// Props o properties: Son una forma de enviar datos desde un componente padre a un hijo. Las props son una de las características fundamentales de React y son utilizadas para comunicar y compartir información entre componentes.
+// Todos los datos dentro de las llaves {} son las propiedades que un padre le envia a un hijo.
+// Las props se reciben antes de que se renderice el componente
+// En este ejemplo las propiedades que recibe el componente UserCard son username, email, avatarUrl.
+// Por si en algun momento a mi se me olvida mandar las propiedades de un padre a un hijo, lo que puedo hacer es setear valores por defecto por si en algun momento los llego a necesitar.
+// La forma de hacer eso, es a la par de la propiedad, poner un igual y luego poner un valor por defecto.
+
+const UserCard = ({
+  username = "default",
+  email = "default@hotmail.com",
+  avatarUrl = "https://random.png",
+  arreglo = [],
+}) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-4 max-w-xs mx-auto">
       <div className="flex items-center justify-center">
